6c9535f75c6a5f8d9964e910789e2fabdc6350b5
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2023-10-05 09:14:19 -0700

    Now returns the json of the server and the server list

--- get-all-servers.js
+++ get-all-servers.js
@@ -10,13 +10,41 @@ export async function main(ns) {
   function all_nodes() {
     let seen = ["home"];
     let unseen = ns.scan("home");
+
     for (let i = 0; i < unseen.length; i++) {
       unseen = unseen.concat(ns.scan(unseen[i])).filter((s) => !(seen.includes(s))).flat(Infinity);
-      seen.push(unseen[i])
+      seen.push(unseen[i]);
       unseen = removeItemFromList(i,unseen);
       i -= 1;
     }
-    return seen;
+
+    seen = removeItemFromList(0,seen);
+    let servers = {};
+
+    seen.forEach(s => {
+      servers[s] = {};
+      servers[s].rootAccess = ns.hasRootAccess(s);
+      servers[s].availableMoney = ns.getServerMoneyAvailable(s);
+      servers[s].maxMoney = ns.getServerMaxMoney(s);
+      servers[s].securityLevel = ns.getServerSecurityLevel(s);
+      servers[s].minSecurityLevel = ns.getServerMinSecurityLevel(s);
+      servers[s].minHackLevel = ns.getServerRequiredHackingLevel(s);
+      servers[s].numPorts = ns.getServerNumPortsRequired(s);
+      servers[s].maxRam = ns.getServerMaxRam(s);
+      servers[s].usedRam = ns.getServerUsedRam(s);
+      servers[s].files = ns.ls(s);
+      servers[s].processes = ns.ps(s);
+    });
+
+
+    let toReturn = {};
+    toReturn.servers = seen;
+    toReturn.serverData = servers;
+    return toReturn
   }
+
+  /* example usage
+  ns.print(`Servers: ${all_nodes().servers.join(", ")}\nN00dle's data: ${all_nodes().serverData.n00dles.maxRam}`);
+  */
 
 }
