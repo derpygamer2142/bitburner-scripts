71e7a6708d08a8d7f386bddac46ce228bd3ee9e1
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2023-10-04 17:42:19 -0700

    Update excessively-complicated.js

--- excessively-complicated.js
+++ excessively-complicated.js
@@ -1,21 +1,36 @@
 /** @param {NS} ns */
-
 export async function main(ns) {
-  
-  function all_nodes_from(server, ignore) {
-    let nodes = ns.scan(server);
-    let to_add = [];
-    nodes.forEach(s => {
-      if (!(ignore.includes(s))) {
-        ignore.push(s);
-        to_add.push(s)
-        to_add.push(all_nodes_from(s, ignore)[0].flat(Infinity));
-      }
-    });
-    // ns.print(`Node ${server} complete.`);
-    return [nodes.concat(to_add).flat(Infinity), ignore];
 
+  function all_nodes_from(server, ignore) {
+    if (!(ignore.includes(server))) {
+
+      let nodes = ns.scan(server);
+      nodes = nodes.filter((s) => !(ignore.includes(s)));
+      let to_add = [];
+
+
+      nodes.forEach(s => {
+
+
+
+        ignore.push(s);
+        to_add.push(s);
+        let e = all_nodes_from(s, ignore);
+
+        if (!(e == undefined)) {
+
+          to_add.push(e[0].flat(Infinity));
+          ignore.push(e[1].flat(Infinity));
+        }
+          
+
+      });
+
+
+      // ns.print(`Node ${server} complete.`);
+      return [nodes.concat(to_add).flat(Infinity), ignore];
+    }
   }
 
-  ns.print(all_nodes_from("home", []))
+  ns.print(all_nodes_from("home", []));
 }
