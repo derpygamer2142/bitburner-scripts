42fcba26b18b27e338422225f2737c248384c423
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2023-10-04 17:17:28 -0700

    Update excessively-complicated.js

--- excessively-complicated.js
+++ excessively-complicated.js
@@ -1,21 +1,21 @@
+/** @param {NS} ns */
+
 export async function main(ns) {
   
-  function all_nodes_from(server,ignore) {
+  function all_nodes_from(server, ignore) {
     let nodes = ns.scan(server);
     let to_add = [];
     nodes.forEach(s => {
       if (!(ignore.includes(s))) {
         ignore.push(s);
         to_add.push(s)
-        let e = all_nodes_from(s,ignore)
-        to_add.push(e[0].flat(Infinity));
-        ignore.push(e[1].flat(Infinity))
+        to_add.push(all_nodes_from(s, ignore)[0].flat(Infinity));
       }
     });
     // ns.print(`Node ${server} complete.`);
-    return [nodes.concat(to_add).flat(Infinity),ignore];
+    return [nodes.concat(to_add).flat(Infinity), ignore];
 
   }
 
-  ns.print(all_nodes_from("home",[]))
+  ns.print(all_nodes_from("home", []))
 }
